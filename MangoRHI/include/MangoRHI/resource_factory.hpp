4a2037ed56c892e2be8b906c6a1fa909163a5ca0
yqs <1697427528@qq.com>
2023-06-11 16:15:15 +0800

    remove ResourceDeleter

--- MangoRHI/include/MangoRHI/resource_factory.hpp
+++ MangoRHI/include/MangoRHI/resource_factory.hpp
@@ -9,21 +9,14 @@
 #include "resource/shader_program.hpp"
 
 namespace MangoRHI {
-    template <typename T>
-    struct ResourceDeleter {
-        void operator()(T *resource) const {
-            resource->destroy();
-        }
-    };
-
     class ResourceFactory : public RuntimeComponent {
     public:
-        virtual std::unique_ptr<Shader, ResourceDeleter<Shader>> create_shader(const char *filename) = 0;
-        virtual std::unique_ptr<VertexBuffer, ResourceDeleter<VertexBuffer>> create_vertex_buffer(u32 vertex_size, u32 count = 1024 * 1024) = 0;
-        virtual std::unique_ptr<IndexBuffer, ResourceDeleter<IndexBuffer>> create_index_buffer(u32 count = 1024 * 1024) = 0;
-        virtual std::unique_ptr<Texture, ResourceDeleter<Texture>> create_texture(const char *filename, u32 mipmap_levels = 0) = 0;
-        virtual std::unique_ptr<Texture, ResourceDeleter<Texture>> create_empty_texture(u32 mipmap_levels = 0) = 0;
-        virtual std::unique_ptr<Sampler, ResourceDeleter<Sampler>> create_sampler() = 0;
-        virtual std::unique_ptr<ShaderProgram, ResourceDeleter<ShaderProgram>> create_shader_program(const char *subpass_name) = 0;
+        virtual std::unique_ptr<Shader> create_shader(const char *filename) = 0;
+        virtual std::unique_ptr<VertexBuffer> create_vertex_buffer(u32 vertex_size, u32 count = 1024 * 1024) = 0;
+        virtual std::unique_ptr<IndexBuffer> create_index_buffer(u32 count = 1024 * 1024) = 0;
+        virtual std::unique_ptr<Texture> create_texture(const char *filename, u32 mipmap_levels = 0) = 0;
+        virtual std::unique_ptr<Texture> create_empty_texture(u32 mipmap_levels = 0) = 0;
+        virtual std::unique_ptr<Sampler> create_sampler() = 0;
+        virtual std::unique_ptr<ShaderProgram> create_shader_program(const char *subpass_name) = 0;
     };
 }
--- MangoRHI/src/vulkan/vulkan_resource_factory.hpp
+++ MangoRHI/src/vulkan/vulkan_resource_factory.hpp
@@ -11,13 +11,13 @@
 namespace MangoRHI {
     class VulkanResourceFactory final : public ResourceFactory {
     public:
-        std::unique_ptr<Shader, ResourceDeleter<Shader>> create_shader(const char *filename) override;
-        std::unique_ptr<VertexBuffer, ResourceDeleter<VertexBuffer>> create_vertex_buffer(u32 vertex_size, u32 count) override;
-        std::unique_ptr<IndexBuffer, ResourceDeleter<IndexBuffer>> create_index_buffer(u32 count) override;
-        std::unique_ptr<Texture, ResourceDeleter<Texture>> create_texture(const char *filename, u32 mipmap_levels) override;
-        std::unique_ptr<Texture, ResourceDeleter<Texture>> create_empty_texture(u32 mipmap_levels) override;
-        std::unique_ptr<Sampler, ResourceDeleter<Sampler>> create_sampler() override;
-        std::unique_ptr<ShaderProgram, ResourceDeleter<ShaderProgram>> create_shader_program(const char *subpass_name) override;
+        std::unique_ptr<Shader> create_shader(const char *filename) override;
+        std::unique_ptr<VertexBuffer> create_vertex_buffer(u32 vertex_size, u32 count) override;
+        std::unique_ptr<IndexBuffer> create_index_buffer(u32 count) override;
+        std::unique_ptr<Texture> create_texture(const char *filename, u32 mipmap_levels) override;
+        std::unique_ptr<Texture> create_empty_texture(u32 mipmap_levels) override;
+        std::unique_ptr<Sampler> create_sampler() override;
+        std::unique_ptr<ShaderProgram> create_shader_program(const char *subpass_name) override;
 
     declare_component_cls(VulkanResourceFactory)
     };
